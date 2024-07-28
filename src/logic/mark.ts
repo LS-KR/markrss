1e8a401b7a5b0256ae994f7b440874a6d305ff69
Elihuso Quigley <Elihuso@outlook.com>
2024-07-28 22:12:13 +0800

    [O] Randomize id

--- src/logic/mark.ts
+++ src/logic/mark.ts
@@ -4,5 +4,9 @@ export function build(title: string, markdown: string): string {
   const content = `<![CDATA[${marked(markdown)}]]>`;
   const summary = `<summary type="html">${title}</summary>`;
   const now = new Date();
-  return `<entry><title>${title}</title><id>${title}</id><content type="html">${content}</content>${summary}<published>${now.toISOString()}</published><updated>${now.toISOString()}</updated></entry>`
+  return `<entry><title>${title}</title><id>${rand(0, 2147483647)}${title}${rand(0, 2147483647)}</id><content type="html">${content}</content>${summary}<published>${now.toISOString()}</published><updated>${now.toISOString()}</updated></entry>`
+}
+
+export function rand(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
